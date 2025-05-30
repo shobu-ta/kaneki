3fdd1e314e971b302a50504417e1a1071e0f0cc6
shobu-ta <196527230+shobu-ta@users.noreply.github.com>
2025-05-30 16:28:04 +0000

    fine-tuning

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,8 +1,5 @@
 class Menu < ApplicationRecord
   has_one_attached :image
   has_many :reviews, dependent: :destroy
-  #def resized_image
-   # image.variant(resize_to_fill: [400, 300]).processed
-  #end
-
+  has_many :reservations, dependent: :destroy 
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,6 +2,7 @@ class Reservation < ApplicationRecord
   belongs_to :user
   has_many :reservation_details, dependent: :destroy
   accepts_nested_attributes_for :reservation_details, reject_if: :all_blank, allow_destroy: true
+  belongs_to :menu
 
   
 end
