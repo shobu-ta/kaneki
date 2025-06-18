c615247822f603f0d13b50cbf740a2595f6fc886
shobu-ta <196527230+shobu-ta@users.noreply.github.com>
2025-06-18 15:14:54 +0000

    ルーティング整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,45 +1,31 @@
 Rails.application.routes.draw do
  
-  namespace :users do
-    get 'reservations/new'
-    get 'reservations/create'
-    get 'reservations/index'
-    get 'reservations/show'
-    get 'reservations/confirm'
-  end
- 
+  # 管理者用の認証機能
   devise_for :admin
 
   # 管理者専用
   namespace :admin do
-    root 'dashboard#index' # 管理者ダッシュボード
-    resources :menus, only: [:index, :new, :create, :edit, :update, :destroy] do# 商品管理
-      resources :reviews, only: [:destroy]  # ネスト
+    root 'dashboard#index' 
+    resources :menus, only: [:index, :new, :create, :edit, :update, :destroy] do
+      resources :reviews, only: [:destroy]  # 商品に紐づくレビュー削除
     end
-
-    resources :users, only: [:index, :show, :destroy] # 会員管理
-    resources :reviews, only: [:index, :destroy] # レビュー管理
-    resources :reservations, only: [:index, :show, :edit, :update, :destroy] # 予約管理
-    resources :blogs, only: [:index, :new, :create, :edit, :update, :destroy, :show] # 管理者ブログ
+    resources :users, only: [:index, :show, :destroy] 
+    resources :reviews, only: [:index, :destroy] 
+    resources :reservations, only: [:index, :show, :edit, :update, :destroy] 
+    resources :blogs, only: [:index, :new, :create, :edit, :update, :destroy, :show] 
   end
-
+  
   # 一般ユーザー用メニュー・ブログ閲覧
   resources :menus, only: [:index, :show]
   resources :blogs, only: [:index, :show]
 
-  # ユーザー用の認証機能
+  # ユーザーの認証機能
   devise_for :users
 
-  # ユーザー専用の機能
+  # ユーザー専用
   namespace :users do
-    # users controller
-    get 'mypage' => "users#show"
-    get 'mypage/edit' => "users#edit"
-    patch 'mypage' => "users#update"
-    delete 'mypage' => "users#destroy"
-    # ----------------------------
+    resource :mypage, only: [:show, :edit, :update, :destroy], controller: 'users' # users/controllerで機能
     resources :reviews, only: [:index]
-
     resources :reservations, only: [:new, :create, :index, :show, :destroy] do
       collection do
         get :confirm
@@ -54,9 +40,7 @@ Rails.application.routes.draw do
 
   # 固定ページのルーティング
   get 'home/about'
-  root 'home#top' # トップページ
-
-  get 'weekly_menus', to: 'menus#weekly', as: 'weekly_menus' # 今週のMENUページ
-  #get 'blogs', to: 'blogs#index', as: 'blogs' # BLOG一覧ページ
+  root 'home#top' 
+  get 'weekly_menus', to: 'menus#weekly', as: 'weekly_menus' 
   
 end
