d2c5879d1094e610c6320760d282a82d725eb703
shobu-ta <196527230+shobu-ta@users.noreply.github.com>
2025-08-18 13:16:41 +0000

    menuバリデーション

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,4 +2,8 @@ class Menu < ApplicationRecord
   has_one_attached :image
   has_many :reviews, dependent: :destroy
   has_many :reservation_details, dependent: :destroy
+  
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Kaneki
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
